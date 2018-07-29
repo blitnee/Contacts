07f62a50ac6007bc1145710b562bb00bc2b91b91
blitnee <brittneelc@gmail.com>
2018-07-29 13:15:45 -0400

    bug: Add handle field to contact form

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -23,8 +23,9 @@ class CreateContact extends Component {
 						maxHeight={64}
 					/>
 					<div className='create-contact-details'>
-						<input type='text' name='name' placeholder='Name' />
+						<input type='text' name='name' placeholder='First Last' />
 						<input type='text' name='email' placeholder='Email' />
+						<input type='text' name='handle' placeholder='@handle' />
 						<button>Add Contact</button>
 					</div>
 				</form>
